c66b7a3b37d31e79deab4665d556b62609cc3388
WilimGPT <wilim@learncube.com>
2024-10-30 13:42:40 +0100

    Create app.vue

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>AI Teacher Assistant</h1>
+    <PromptCreator />
+  </div>
+</template>
+
+<script>
+import PromptCreator from './components/PromptCreator.vue';
+
+export default {
+  components: {
+    PromptCreator,
+  },
+};
+</script>
+
+<style>
+/* Add any global styles here */
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  color: #333;
+}
+</style>
