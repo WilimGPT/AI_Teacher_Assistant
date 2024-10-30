70367cfdc2319f0b878c200ebf043a8ed633b4a0
WilimGPT <wilim@learncube.com>
2024-10-30 15:02:15 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <template>
   <div id="app">
     <h1>AI Teacher Assistant</h1>
-    <PromptCreator />
   </div>
 </template>
 
 <script>
-import PromptCreator from './components/PromptCreator.vue';
-
 export default {
-  components: {
-    PromptCreator
-  }
+  name: 'App',
 };
 </script>
-
-<style>
-/* Global styles can go here */
-</style>
